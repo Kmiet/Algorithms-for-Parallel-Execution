0cbc54e44b596bf0ddbc3dab1a1168b916c74ed0
Kmiet <krzys.kmiecik@gmail.com>
2020-12-04 23:58:59 +0100

    Adjustments v2

--- lab5/fib-master.py
+++ lab5/fib-master.py
@@ -3,18 +3,27 @@ from mpi4py import MPI
 import numpy
 import sys
 
-comm = MPI.COMM_SELF.Spawn(sys.executable, args=['./fib-worker.py'], maxprocs=32)
+argc = len(sys.argv)
+if argc != 2:
+  print("Missing argument - N. Usage: fib-master.py <n>\n")
+  exit(1)
+
+# Nth fib number
+N = int(sys.argv[1])
+
+comm = MPI.COMM_SELF.Spawn(sys.executable, args=['fib-worker.py'])
 
 start_time = MPI.Wtime()
 
-N = numpy.array(3, 'i')
+N = numpy.array(N, 'i')
 comm.Bcast([N, MPI.INT], root=MPI.ROOT)
 
 FIB = numpy.array(0.0, 'i')
 comm.Reduce(None, [FIB, MPI.INT], op=MPI.SUM, root=MPI.ROOT)
 
 end_time = MPI.Wtime()
-print(FIB)
-print(end_time - start_time)
+# print(FIB)
+print(N, end_time - start_time)
 
 comm.Disconnect()
+exit(1)
--- lab5/fib-worker.py
+++ lab5/fib-worker.py
@@ -6,27 +6,32 @@ import sys
 comm = MPI.Comm.Get_parent()
 size = comm.Get_size()
 rank = comm.Get_rank()
+rank_world = MPI.COMM_WORLD.Get_rank()
 
 N = numpy.array(0, dtype='i')
 
 comm.Bcast([N, MPI.INT], root=0)
-   
-if (N < 2) 
-  comm.Reduce([N, MPI.INT], None, op=MPI.SUM, root=0)
-else {
-  sub_comm1 = MPI.COMM_SELF.Spawn(sys.executable, args=['fib-worker.py'], maxprocs=32)
-  sub_comm2 = MPI.COMM_SELF.Spawn(sys.executable, args=['fib-worker.py'], maxprocs=32)
 
-  sub_comm1.Bcast([N - 1, MPI.INT], root=MPI.ROOT)
-  sub_comm2.Bcast([N - 2, MPI.INT], root=MPI.ROOT)
+if (N < 3):
+  ONE = numpy.array(1, dtype='i')
+  comm.Reduce([ONE, MPI.INT], None, op=MPI.SUM, root=0)
+else:
+  sub_comm1 = MPI.COMM_SELF.Spawn(sys.executable, args=['fib-worker.py'])
+  N_LESS_ONE = numpy.array(N - 1, dtype='i')
+  sub_comm1.Bcast([N_LESS_ONE, MPI.INT], root=MPI.ROOT)
+
+  sub_comm2 = MPI.COMM_SELF.Spawn(sys.executable, args=['fib-worker.py'])
+  N_LESS_TWO = numpy.array(N - 2, dtype='i')
+  sub_comm2.Bcast([N_LESS_TWO, MPI.INT], root=MPI.ROOT)
 
   X = numpy.array(0.0, 'i')
-  X = sub_comm1.Reduce(None, [X, MPI.INT], op=MPI.SUM, root=MPI.ROOT)
+  sub_comm1.Reduce(None, [X, MPI.INT], op=MPI.SUM, root=MPI.ROOT)
 
   Y = numpy.array(0.0, 'i')
-  Y = sub_comm2.Reduce(None, [Y, MPI.INT], op=MPI.SUM, root=MPI.ROOT)
+  sub_comm2.Reduce(None, [Y, MPI.INT], op=MPI.SUM, root=MPI.ROOT)
 
-  comm.Reduce([X + Y, MPI.INT], None, op=MPI.SUM, root=0)
-}
+  FIB_N = numpy.array(X + Y, 'i')
+  comm.Reduce([FIB_N, MPI.INT], None, op=MPI.SUM, root=0)
 
 comm.Disconnect()
+exit(1)
